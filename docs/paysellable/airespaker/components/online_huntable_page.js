e2c57cadad412ebde1ec8662479594d3d3be2a20
attaceph <attaceph@protonmail.com>
2026-08-14 16:20:52 +0700

    Add files via upload

---
+++ docs/paysellable/airespaker/components/online_huntable_page.js
@@ -0,0 +1,141 @@
+/*
+ * Copyright (c) 2026 Dinh Thoai Tran <attaceph@protonmail.com>
+ * All rights reserved.
+ *
+ * License: GPL v.2
+ * Source: https://github.com/attaceph/airespaker
+ *
+ */
+
+const gv_online_page_text = `  __ _(_)  _ _ ___ ____ __   __ _\| \|_____ _ _ 
+ / _\` \| \| \| '_/ -_\|_-< '_ \\ / _\` \| / / -_) '_\|
+ \\__,_\|_\| \|_\| \\___/__/ .__/ \\__,_\|_\\_\\___\|_\|  
+=====================\|_\|=======================
+              AI Response Taker
+===============================================
+
+`;
+
+const gv_online_page_text_2 = `
+`;
+
+const HuntableOnlinePage = {
+  template: `<div v-show="online && page == 'home'" class="online-page"><div class="online-page-inner">=======_==========================<span v-on:click="hide=false">_</span>============<br/>{{ online_page_text }}
+
+<input type="button" class="online-button-2" @click="go_page('login')" value="Login" /> &nbsp; <input  v-show="premium == ''" type="button" class="online-button-2" @click="go_page('register')" value="Register" />
+</div>
+
+  </div>
+  </div>
+  </div>
+  <login_page v-show="online && page == 'login'" ref="login_page" @go_page="go_page" @set_token="set_token" @set_username="set_username"></login_page>  
+  <dashboard_page v-show="online && page == 'dashboard'" ref="dashboard_page" @go_page="go_page" @set_token="set_token"></dashboard_page>  
+  <take_page v-show="online && page == 'take'" ref="take_page" @go_page="go_page" @set_token="set_token"></take_page>  
+  <profile_page v-show="online && page == 'profile'" ref="profile_page" @go_page="go_page" @set_token="set_token"></profile_page>  
+  <register_page v-show="online && page == 'register'" ref="register_page" @go_page="go_page" @set_token="set_token"></register_page>  
+  <aircache_page v-show="online && page == 'aircache'" ref="aircache_page" @go_page="go_page" @set_token="set_token"></aircache_page>  
+  <savecache_page v-show="online && page == 'savecache'" ref="savecache_page" @go_page="go_page" @set_token="set_token"></savecache_page>  
+`,
+  emits: [ 'update_online' ],
+  data() {
+    return {
+      premium: go_premium,
+      username: go_premium,
+      page: 'home',
+      token: '',
+      online_page_text: gv_online_page_text,
+      online_page_text_2: gv_online_page_text_2,
+      online: false,
+      hide: go_enable_prelaunch
+    };
+  },
+  methods: {
+    update_online( value ) {
+      this.online = value;
+      
+      if (this.premium === '') {
+        let uri = location + '';
+        uri = uri.replaceAll('https://airespaker.is-best.net', '').replaceAll('http://airespaker.is-best.net', '');
+        let qry = '';
+        let lidx = uri.lastIndexOf('/');
+        if (lidx >= 0) {
+          lidx = uri.indexOf('q=', lidx + 1);
+          if (lidx >= 0) {
+            qry = uri.substring(lidx + 2);
+            lidx = qry.indexOf('&');
+            if (lidx >= 0) {
+              qry = qry.substring(0, lidx);
+            }
+            qry = decodeURIComponent(qry);
+          }
+        }
+        let idx = uri.indexOf('/c/');
+        if (idx === 0) {
+          location = 'https://respache.is-best.net';
+          return;
+
+          uri = uri.substring(3);
+          let username = 'airespaker';
+          idx = uri.indexOf('/');
+          if (idx >= 0) {
+            username = uri.substring(0, idx);
+          }
+          if (this.page !== 'aircache') {
+            this.go_page('aircache');
+            if (qry.length > 0) {
+              this.$refs.aircache_page.setUsername(username, false);   
+              this.$refs.aircache_page.setQuery(qry);   
+            } else {
+              this.$refs.aircache_page.setUsername(username, true);   
+            }
+          }
+        } else if (this.token === '' && uri.indexOf('/register/') === 0) {
+          this.go_page('register');
+        } else if (this.token === '' && uri.indexOf('/login/') === 0) {
+          this.go_page('login');
+        }
+      }
+    },
+    set_username( username ) {
+      if (this.token !== '') {
+        this.username = username;
+      } else {
+        this.username = '';
+      }
+    },
+    set_token( value ) {
+      if (value === '' && this.token !== '') {
+        gj_text_post( '/airespaker/index.php?method=logout', {'token': this.token}, 'n', function( text ) {
+        });      
+      }
+      this.token = value;
+    },
+    go_page( value ) {
+      if ( value == 'login' ) {
+        this.$refs.login_page.doPrepare();
+      } else if ( value == 'take' ) {
+        this.$refs.take_page.doPrepare(this.token);      
+      } else if ( value == 'register' ) {
+        this.$refs.register_page.doPrepare();      
+      } else if ( value == 'profile' ) {
+        this.$refs.profile_page.doPrepare(this.token);      
+      } else if ( value == 'dashboard' ) {
+        this.$refs.dashboard_page.doPrepare(this.token);      
+      } else if ( value == 'aircache' ) {
+        window.open( 'https://respache.is-best.net', '_blank');
+        return;
+        this.$refs.aircache_page.doPrepare(this.token, this.username);      
+      } else if ( value == 'savecache' ) {
+        window.open( 'https://respache.is-best.net', '_blank');
+        return;
+        this.$refs.savecache_page.doPrepare(this.token, this.username);      
+      }
+      this.page = value;
+    },
+    doLogout() {
+      gj_text_post( '/airespaker/index.php?method=logout', {'token': this.token}, 'n', function( text ) {
+      });      
+      this.token = '';
+    }
+  }
+};
